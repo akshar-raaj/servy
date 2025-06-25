84b1bc128c63d305d71b72598e3145215ae402d2
Akshar Raaj <akshar.raaj@sentientsolutions.io>
2025-06-26 00:02:19 +0530

    Removed hardcoded values. Update map with resp_body and add a dynamic Content-Length

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -18,17 +18,16 @@ defmodule Servy.Handler do
   end
 
   def route(conv) do
-    # TODO: Create a new map that also has the response body
-    conv = %{method: "GET", path: "/wildthings", resp_body: "tiger, lion, wolf"}
+    %{conv | resp_body: "Tiger, Lion, Wolf"}
   end
 
   def format_response(conv) do
     """
     HTTP/1.1 200 OK
     Content-Type: text/html
-    Content-Length: 20
+    Content-Length: #{String.length conv.resp_body}
 
-    tiger, lion, wolf
+    #{conv.resp_body}
     """
   end
 end
