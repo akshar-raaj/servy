e0df9bdd4d1a944f670ab039c9acb5f26607c970
Akshar Raaj <akshar.raaj@sentientsolutions.io>
2025-06-26 11:07:52 +0530

    Return appropriate status code and reason when a path is not supported.

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -18,7 +18,7 @@ defmodule Servy.Handler do
       |> String.split("\n")
       |> List.first()
       |> String.split(" ")
-    %{method: method, path: path, resp_body: ""}
+    %{method: method, path: path, status: nil, resp_body: ""}
   end
 
   def route(conv) do
@@ -26,25 +26,33 @@ defmodule Servy.Handler do
   end
 
   def route(conv, "GET", "/wildthings") do
-    %{conv | resp_body: "Tiger, Lion, Wolf"}
+    %{conv | status: 200, resp_body: "Tiger, Lion, Wolf"}
   end
 
   def route(conv, "GET", "/bears") do
-    %{conv | resp_body: "Bears"}
+    %{conv | status: 200, resp_body: "Bears"}
   end
 
   def route(conv, _method, path) do
-    %{conv | resp_body: "Not Found: #{path}"}
+    %{conv | status: 404, resp_body: "Not Found: #{path}"}
   end
 
   def format_response(conv) do
     """
-    HTTP/1.1 200 OK
+    HTTP/1.1 #{conv.status} #{status_response conv.status}
     Content-Type: text/html
     Content-Length: #{byte_size conv.resp_body}
 
     #{conv.resp_body}
     """
+  end
+
+  defp status_response(code) do
+    %{
+      200=> "OK",
+      404=> "Not Found",
+      201=> "Created"
+    }[code]
   end
 end
 
