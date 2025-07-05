a436323097afc36a506c24a200ca1c99815562dc
Akshar Raaj <akshar.raaj@sentientsolutions.io>
2025-07-06 01:05:00 +0530

    Revise fundamental Elixir constructs

--- lib/servy.ex
+++ lib/servy.ex
@@ -1,6 +1,17 @@
 defmodule Servy do
+
+  @moduledoc "A module to try things out as I learn Elixir!"
+
+  def first_token(str) do
+    str |> String.split("\n") |> List.first() |> String.split(" ") |> List.first()
+  end
+
+  def hello("akshar") do
+    "Hello, I am Akshar!"
+  end
+
   def hello(name) do
-    "Hello, #{name}!"
+    "Hello, #{name}! I am a catch all."
   end
 
   def double(x) do
@@ -10,4 +21,40 @@ defmodule Servy do
   def four_times(x) do
     x |> double |> double
   end
+
+  def lname(%{name: "akshar"}) do
+    "raaj"
+  end
+
+  def lname(%{name: "neelima"}) do
+    "gupta"
+  end
+
+  def lname(map) do
+    "unknown"
+  end
+
+  def age(map) do
+    case map.name do
+      "akshar" -> 36
+      "neelima" -> 35
+      "apaar" -> 6
+      "struva" -> 0.1
+    end
+  end
 end
+
+IO.puts Servy.hello("akshar raaj")
+IO.puts Servy.first_token("""
+GET /wildlife HTTP/1.1
+Host: example.com
+Accept: */*
+User-Agent: Elixir Client
+""")
+
+IO.puts Servy.hello("akshar")
+
+IO.puts Servy.hello("neelima")
+
+IO.puts Servy.lname(%{name: "akshar", age: 36})
+IO.puts Servy.lname(%{name: "neelima gupta", age: 36})
